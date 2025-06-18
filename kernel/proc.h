885c34f20515aa4a04fcf84db4ee66979ab5bd4c
Mohammad Javad <mohamadjavad.deh1384@gmail.com>
2025-06-18 14:32:36 +0330

    Initial setup for multi-threading: updated Makefile, param.h, proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -20,10 +20,10 @@ struct context {
 
 // Per-CPU state.
 struct cpu {
-  struct proc *proc;          // The process running on this cpu, or null.
-  struct context context;     // swtch() here to enter scheduler().
-  int noff;                   // Depth of push_off() nesting.
-  int intena;                 // Were interrupts enabled before push_off()?
+  struct proc *proc;        // The process running on this cpu, or null.
+  struct context context;   // swtch() here to enter scheduler().
+  int noff;                 // Depth of push_off() nesting.
+  int intena;               // Were interrupts enabled before push_off()?
 };
 
 extern struct cpu cpus[NCPU];
@@ -41,19 +41,19 @@ extern struct cpu cpus[NCPU];
 // return-to-user path via usertrapret() doesn't return through
 // the entire kernel call stack.
 struct trapframe {
-  /*   0 */ uint64 kernel_satp;   // kernel page table
-  /*   8 */ uint64 kernel_sp;     // top of process's kernel stack
-  /*  16 */ uint64 kernel_trap;   // usertrap()
-  /*  24 */ uint64 epc;           // saved user program counter
-  /*  32 */ uint64 kernel_hartid; // saved kernel tp
-  /*  40 */ uint64 ra;
-  /*  48 */ uint64 sp;
-  /*  56 */ uint64 gp;
-  /*  64 */ uint64 tp;
-  /*  72 */ uint64 t0;
-  /*  80 */ uint64 t1;
-  /*  88 */ uint64 t2;
-  /*  96 */ uint64 s0;
+  /* 0 */ uint64 kernel_satp;   // kernel page table
+  /* 8 */ uint64 kernel_sp;     // top of process's kernel stack
+  /* 16 */ uint64 kernel_trap;   // usertrap()
+  /* 24 */ uint64 epc;           // saved user program counter
+  /* 32 */ uint64 kernel_hartid; // saved kernel tp
+  /* 40 */ uint64 ra;
+  /* 48 */ uint64 sp;
+  /* 56 */ uint64 gp;
+  /* 64 */ uint64 tp;
+  /* 72 */ uint64 t0;
+  /* 80 */ uint64 t1;
+  /* 88 */ uint64 t2;
+  /* 96 */ uint64 s0;
   /* 104 */ uint64 s1;
   /* 112 */ uint64 a0;
   /* 120 */ uint64 a1;
@@ -79,6 +79,28 @@ struct trapframe {
   /* 280 */ uint64 t6;
 };
 
+// --- Start of new thread-related definitions ---
+
+enum threadstate {
+  THREAD_UNUSED,    // Thread slot is unused
+  THREAD_RUNNABLE,  // Thread is ready to run
+  THREAD_RUNNING,   // Thread is currently running
+  THREAD_JOINED,    // Thread is waiting for another thread to finish
+  THREAD_SLEEPING   // Thread is sleeping
+};
+
+struct thread {
+  enum threadstate state;      // Thread state
+  struct trapframe *trapframe; // Trapframe for this thread
+  uint id;                     // Thread ID
+  uint join;                   // Thread ID to join with (for jointhread)
+  int sleep_n;                 // Number of ticks to sleep
+  uint sleep_tick0;            // Tick when sleep started
+};
+
+// --- End of new thread-related definitions ---
+
+
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
@@ -86,22 +108,27 @@ struct proc {
   struct spinlock lock;
 
   // p->lock must be held when using these:
-  enum procstate state;        // Process state
-  void *chan;                  // If non-zero, sleeping on chan
-  int killed;                  // If non-zero, have been killed
-  int xstate;                  // Exit status to be returned to parent's wait
-  int pid;                     // Process ID
+  enum procstate state;         // Process state
+  void *chan;                   // If non-zero, sleeping on chan
+  int killed;                   // If non-zero, have been killed
+  int xstate;                   // Exit status to be returned to parent's wait
+  int pid;                      // Process ID
 
   // wait_lock must be held when using this:
-  struct proc *parent;         // Parent process
+  struct proc *parent;          // Parent process
 
   // these are private to the process, so p->lock need not be held.
-  uint64 kstack;               // Virtual address of kernel stack
-  uint64 sz;                   // Size of process memory (bytes)
-  pagetable_t pagetable;       // User page table
-  struct trapframe *trapframe; // data page for trampoline.S
-  struct context context;      // swtch() here to run process
-  struct file *ofile[NOFILE];  // Open files
-  struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
+  uint64 kstack;                // Virtual address of kernel stack
+  uint64 sz;                    // Size of process memory (bytes)
+  pagetable_t pagetable;        // User page table
+  struct trapframe *trapframe;  // data page for trampoline.S
+  struct context context;       // swtch() here to run process
+
+  // Thread-specific fields added for multi-threading:
+  struct thread threads[NTHREAD];   // Array of threads belonging to this process
+  struct thread *current_thread;    // Pointer to the currently running thread within this process
+
+  struct file *ofile[NOFILE];   // Open files
+  struct inode *cwd;            // Current directory
+  char name[16];                // Process name (debugging)
 };
